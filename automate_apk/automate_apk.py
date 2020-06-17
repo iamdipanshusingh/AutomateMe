122be7717cc04b2f9d188b55b1a24fdb1bb5af64
Dipanshu Singh <dipanshu.singh@civilmachines.com>
2020-06-18 01:11:15 +0530

    Adds - KeyboardInterrupt exception

--- automate_apk/automate_apk.py
+++ automate_apk/automate_apk.py
@@ -6,18 +6,21 @@ try:
 except:
     pass
 
-app_name = input("Enter app's name: ")
-app_version = input("Enter app's version: ")
-pubspec_path = input("Enter app's root folder location with respect to current folder: ")
+try:
+    app_name = input("Enter app's name: ")
+    app_version = input("Enter app's version: ")
+    pubspec_path = input("Enter app's root folder location with respect to current folder: ")
 
-path = pubspec_path + "build/app/outputs/apk/release/app-release.apk"
+    path = pubspec_path + "build/app/outputs/apk/release/app-release.apk"
 
-apk_name = app_name + '-v' + app_version
+    apk_name = app_name + '-v' + app_version
 
-os.system("flutter clean; flutter pub get;")
-os.system("echo [+] Cleaned cache and ran 'flutter pub get'")
-os.system("echo [+] Creating apk")
-os.system("flutter build apk")
-os.system("cp " + path + " ./" + app_name + '.apk')
-os.system("echo [+] Apk created")
-os.system("nautilus .")
+    os.system("flutter clean; flutter pub get;")
+    os.system("echo [+] Cleaned cache and ran 'flutter pub get'")
+    os.system("echo [+] Creating apk")
+    os.system("flutter build apk")
+    os.system("cp " + path + " ./" + app_name + '.apk')
+    os.system("echo [+] Apk created")
+    os.system("nautilus .")
+except KeyboardInterrupt:
+    os.system("echo '\n[.] Terminating the process'")
