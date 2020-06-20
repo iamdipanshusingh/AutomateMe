1c9617329340c28552f04a030118bf00bfa30e1b
Dipanshu Singh <dipanshu.singh@civilmachines.com>
2020-06-20 14:59:22 +0530

    Changed the app name convention and adds a notification feature

--- automate_apk/automate_apk.py
+++ automate_apk/automate_apk.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import os
 import glob
+import platform
+import contextlib
+from pydub import AudioSegment
+from pydub.playback import play
 
 try:
     input = raw_input
@@ -8,8 +12,7 @@ except:
     pass
 
 try:
-    app_name = input("Enter app's name: ")
-    app_version = input("Enter app's version: ")
+    app_name = input("What do you want to call your app ? ")
     pubspec_path = input("Enter app's root folder location with respect to current folder: ")
 
     for apk in glob.glob("*.apk"):
@@ -21,14 +24,17 @@ try:
 
     path = pubspec_path + "build/app/outputs/apk/release/app-release.apk"
 
-    apk_name = app_name + '-v' + app_version
-
     os.system("flutter clean; flutter pub get;")
     os.system("echo [+] Cleaned cache and ran 'flutter pub get'")
     os.system("echo [+] Creating apk")
     os.system("flutter build apk")
     os.system("cp " + path + " ./" + app_name + '.apk')
     os.system("echo [+] Apk created")
+
+    sound = AudioSegment.from_mp3('accomplished.mp3')
+    play(sound)
+    
     os.system("nautilus .")
+
 except KeyboardInterrupt:
     os.system("echo '\n[.] Terminating the process'")
